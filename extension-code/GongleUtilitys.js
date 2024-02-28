5eb7899b6943d354dabd8980603989efe3778b9d
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-02-28 12:54:54 -0700

    Update dis

--- extension-code/GongleUtilitys.js
+++ extension-code/GongleUtilitys.js
@@ -28,9 +28,63 @@
           },
           {
             opcode: 'mobile',
-            blockType: Scratch.BlockType.REPORTER,
+            blockType: Scratch.BlockType.BOOLEAN,
             text: 'On moblie device?',
             disableMonitor: true,
+          },
+          {
+            opcode: 'super',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[STR] === [STG]',
+            disableMonitor: true,
+            arguments: {
+              STR: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hello"
+              },
+              STG: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "hello"
+              },
+            }
+          },
+          {
+            opcode: 'fake',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'true?',
+            disableMonitor: true,
+          },
+          {
+            opcode: 'text',
+            blockType: Scratch.BlockType.REPORTER,
+            text: '"[STR]"',
+            disableMonitor: true,
+            arguments: {
+              STR: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hello, world!"
+              },
+            }
+          },
+          {
+            opcode: 'replace',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'In [text] replace [STR] with [replacement]',
+            disableMonitor: true,
+            arguments: {
+               text: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "apple, banana, carrot"
+              },
+               STR: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "carrot"
+              },
+               replacement: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "pear"
+              },
+            }
           },
             
         ],
@@ -41,6 +95,18 @@
     }
     mobile(args) {
       return navigator.userAgentData.mobile;
+    }
+    fake(args) {
+      return "Imposter Reporter dressed as a Boolean!!"
+    }
+    super(args) {
+      return args.STR === args.STG
+    }
+    text(args) {
+      return args.STR
+    }
+    replace(args) {
+      return args.text.replaceAll(args.STR, args.replacement)
     }
 
   }
